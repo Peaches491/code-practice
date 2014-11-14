affd5d228bfdc0cf0ec22a6f79778c963e1d1af0
Peaches491 <Peaches491@gmail.com>
2014-11-13 22:15:48 -0500

    Initial functionality implemented

---
+++ test/practice/FixedHashMap/FixedSizeHashMapTest.java
@@ -0,0 +1,13 @@
+package practice.FixedHashMap;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FixedSizeHashMapTest {
+
+	@Test
+	public void test() {
+		assertNotNull("Not yet implemented");
+	}
+
+}
