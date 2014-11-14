e0eb0c855ae421f226340c5e3e2c3f4e38a49b67
Peaches491 <Peaches491@gmail.com>
2014-11-13 23:17:08 -0500

    Preliminary test set defined. All tests passing

--- src/test/java/test/FixedHashMap/FixedSizeHashMapTest.java
+++ src/test/java/test/FixedHashMap/FixedSizeHashMapTest.java
@@ -22,27 +22,76 @@ public class FixedSizeHashMapTest {
 	}
 
 	@Test
-	public void test() {
-
-		HistoryDeletingHashMap<Integer, Integer> integerMap = 
-				new HistoryDeletingHashMap<Integer, Integer>(10);
+	public void testMuchLessThanFull() {
+		int integerMapMax = 100;
+		HistoryDeletingHashMap<Integer, String> integerMap = 
+				new HistoryDeletingHashMap<Integer, String>(integerMapMax);
 		
-		int firstSetSize = 6;
-		
+		int firstSetSize = 25;
 		for(int i = 0; i < firstSetSize; i++){
-			integerMap.put(i, i);
+			integerMap.put(i, ""+i);
 		}
 		
 		assertEquals("After insertion into empty HistoryDeletingHashMap, "
 				+ "size was incorect.", firstSetSize, integerMap.size());
-		
-		int offset = 10;
-		for(int i = 0; i < 5; i++){
-			integerMap.put(offset+i, offset+i);
+
+		int secondSetSize = 5;
+		for(int i = 100; i < 100+secondSetSize; i++){
+			integerMap.put(secondSetSize+i, ""+secondSetSize+i);
 		}
 		
-		integerMap.remove(12);
-		integerMap.remove(7);
+		assertEquals("After insertion into non-empty HistoryDeletingHashMap, "
+				+ "size was incorect.", firstSetSize+secondSetSize, integerMap.size());
+		
+	}
+	
+	@Test
+	public void testExactlyFull() {
+
+		int integerMapMax = 100;
+		HistoryDeletingHashMap<Integer, String> integerMap = 
+				new HistoryDeletingHashMap<Integer, String>(integerMapMax);
+		
+		int firstSetSize = integerMapMax;
+		for(int i = 0; i < firstSetSize; i++){
+			integerMap.put(i, ""+i);
+		}
+		
+		assertEquals("After insertion of <maxSize> elements into empty HistoryDeletingHashMap, "
+				+ "size was incorect.", firstSetSize, integerMap.size());
+
+		int secondSetSize = 5;
+		for(int i = 100; i < 100+secondSetSize; i++){
+			integerMap.put(secondSetSize+i, ""+secondSetSize+i);
+		}
+		
+		assertEquals("After insertion into non-empty HistoryDeletingHashMap, "
+				+ "size was incorect.", integerMapMax, integerMap.size());
+		
+	}
+	
+	@Test
+	public void testOverFull() {
+
+		int integerMapMax = 100;
+		HistoryDeletingHashMap<Integer, String> integerMap = 
+				new HistoryDeletingHashMap<Integer, String>(integerMapMax);
+		
+		int firstSetSize = integerMapMax + 50;
+		for(int i = 0; i < firstSetSize; i++){
+			integerMap.put(i, ""+i);
+		}
+		
+		assertEquals("After insertion of <maxSize> elements into empty HistoryDeletingHashMap, "
+				+ "size was incorect.", integerMapMax, integerMap.size());
+
+		int secondSetSize = 5;
+		for(int i = 200; i < 200+secondSetSize; i++){
+			integerMap.put(secondSetSize+i, ""+secondSetSize+i);
+		}
+		
+		assertEquals("After insertion into non-empty HistoryDeletingHashMap, "
+				+ "size was incorect.", integerMapMax, integerMap.size());
 		
 	}
 
